0d78a330bd7d9be3d74ab5889270d4988b5af569
Hyunwoo <nhw3990@kau.kr>
2025-03-19 22:55:19 +0900

    update: footer 에 링크 리디렉션 추가

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -7,16 +7,14 @@ export default function Footer() {
       <div className="flex items-center relative">
         <img src="/images/icon_with_text.svg" alt="RefHub" className="h-8" />
         <p className="font-bold ml-12">E-mail</p>
-        <p className="font-medium ml-3.5">refhub0@gmail.com</p>
-        <img
-          src={instar}
-          alt="instargram"
-          className="absolute right-0 cursor-pointer"
-        />
+        <a href="mailto:refhub0@gmail.com" className="font-medium ml-3.5 cursor-pointer">refhub0@gmail.com</a>
+        <a href="https://www.instagram.com/_refhub?igsh=b2ppejRrd2F3amQ0&utm_source=qr" target="_blank" rel="noopener noreferrer">
+          <img src={instar} alt="instargram" className="absolute right-0 cursor-pointer" />
+        </a>
       </div>
       <div className="flex font-medium mt-11 relative">
-        <p className="cursor-pointer">이용약관</p>
-        <p className="ml-8 cursor-pointer">개인정보처리방침</p>
+        <a href="https://psychedelic-crustacean-955.notion.site/1bbfa429e6f7804d9782d2b439cb0a29?pvs=4" target="_blank" rel="noopener noreferrer" className="cursor-pointer">이용약관</a>
+        <a href="https://psychedelic-crustacean-955.notion.site/1bbfa429e6f78078a633dac2653b3dec?pvs=4" target="_blank" rel="noopener noreferrer" className="ml-8 cursor-pointer">개인정보처리방침</a>
         <p className="underline absolute right-0 cursor-pointer">탈퇴하기</p>
       </div>
     </footer>
