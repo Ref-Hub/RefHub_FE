34960b0625cabbaba868c8e98de8d633a4498f0b
Hyunwoo <nhw3990@kau.kr>
2025-04-26 02:20:39 +0900

    feat: add state utility functions for Recoil state management
    - Introduced utility functions for creating state updaters, selectors, and subscribers using Recoil.
    - Added memoized selector creation for optimized state transformations.
    - Implemented state initializer for setting default values in Recoil atoms.

---
+++ src/utils/stateUtils.ts
@@ -0,0 +1,57 @@
+import { atom, selector, useRecoilValue, useSetRecoilState } from "recoil";
+import { useCallback } from "react";
+
+// 상태 업데이트 유틸리티
+export const createStateUpdater = <T>(atom: any) => {
+  return () => {
+    const setState = useSetRecoilState(atom);
+    return useCallback(
+      (newValue: T | ((prev: T) => T)) => {
+        setState(newValue);
+      },
+      [setState]
+    );
+  };
+};
+
+// 상태 선택자 유틸리티
+export const createStateSelector = <T>(atom: any) => {
+  return () => useRecoilValue(atom);
+};
+
+// 메모이제이션된 상태 선택자
+export const createMemoizedSelector = <T>(
+  atom: any,
+  transform: (value: T) => any
+) => {
+  return selector({
+    key: `${atom.key}-memoized`,
+    get: ({ get }) => {
+      const value = get(atom);
+      return transform(value);
+    },
+  });
+};
+
+// 상태 초기화 유틸리티
+export const createStateInitializer = <T>(atom: any, defaultValue: T) => {
+  return () => {
+    const setState = useSetRecoilState(atom);
+    return useCallback(() => {
+      setState(defaultValue);
+    }, [setState]);
+  };
+};
+
+// 상태 구독 유틼리티
+export const createStateSubscriber = <T>(
+  atom: any,
+  callback: (value: T) => void
+) => {
+  return () => {
+    const value = useRecoilValue(atom);
+    return useCallback(() => {
+      callback(value);
+    }, [value]);
+  };
+};
